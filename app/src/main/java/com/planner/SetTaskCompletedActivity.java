6b7219cc8b78c15a75a215873b6e397d05d66a31
Kate <ekaterinaicenko@gmail.com>
2021-05-31 19:52:21 +0300

    Add adding images without connection with database

--- app/src/main/java/com/planner/SetTaskCompletedActivity.java
+++ app/src/main/java/com/planner/SetTaskCompletedActivity.java
@@ -1,17 +1,23 @@
 package com.planner;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.squareup.picasso.Picasso;
 
 public class SetTaskCompletedActivity extends AppCompatActivity {
     private String title;
     private String desc;
     private int reward;
     private int pos;
+    private Uri fileImageUri;
+    private ImageView imageFile;
     private boolean activityRes;
 
     @Override
@@ -20,6 +26,8 @@ public class SetTaskCompletedActivity extends AppCompatActivity {
         setContentView(R.layout.activity_set_task_completed);
         ImageView imageSetTaskCompleted = findViewById(R.id.imageChangeStatusTask);
         ImageView imageBack = findViewById(R.id.imageBackSetTaskCompleted);
+        imageFile = findViewById(R.id.imageFile);
+        Button chooseImageButton = findViewById(R.id.chooseFileButton);
 
         title = getIntent().getStringExtra("title");
         desc = getIntent().getStringExtra("desc");
@@ -35,6 +43,13 @@ public class SetTaskCompletedActivity extends AppCompatActivity {
         TextView taskReward = findViewById(R.id.rewardTask);
         taskReward.setText("reward: " + reward);
 
+        chooseImageButton.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(intent, RequestCodes.REQUEST_CODE_ADD_IMAGE);
+        });
+
 
         imageBack.setOnClickListener(v -> onBackPressed());
         imageSetTaskCompleted.setOnClickListener(v -> {
@@ -47,5 +62,15 @@ public class SetTaskCompletedActivity extends AppCompatActivity {
             setResult(RESULT_OK, intent);
             finish();
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == RequestCodes.REQUEST_CODE_ADD_IMAGE) {
+            fileImageUri = data.getData();
+            Picasso.get().load(fileImageUri).into(imageFile);
+            imageFile.setImageURI(fileImageUri);
+        }
     }
 }
