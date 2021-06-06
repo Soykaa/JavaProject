1ec9bd3fcd8d3b3f2989ec5422a0628b6cdc39ff
Kate <ekaterinaicenko@gmail.com>
2021-06-06 16:55:48 +0300

    Add simple storage: all users in one dir

--- app/src/main/java/com/planner/SetTaskCompletedActivity.java
+++ app/src/main/java/com/planner/SetTaskCompletedActivity.java
@@ -1,15 +1,28 @@
 package com.planner;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
+
+import org.jetbrains.annotations.NotNull;
 
 public class SetTaskCompletedActivity extends AppCompatActivity {
     private String title;
@@ -18,7 +31,8 @@ public class SetTaskCompletedActivity extends AppCompatActivity {
     private int pos;
     private Uri fileImageUri;
     private ImageView imageFile;
-    private boolean activityRes;
+    private StorageReference storageReference;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,13 +57,15 @@ public class SetTaskCompletedActivity extends AppCompatActivity {
         TextView taskReward = findViewById(R.id.rewardTask);
         taskReward.setText("reward: " + reward);
 
+        storageReference = FirebaseStorage.getInstance().getReference("uploads");
+        databaseReference = FirebaseDatabase.getInstance().getReference("uploads");
+
         chooseImageButton.setOnClickListener(v -> {
             Intent intent = new Intent();
             intent.setType("image/*");
             intent.setAction(Intent.ACTION_GET_CONTENT);
             startActivityForResult(intent, RequestCodes.REQUEST_CODE_ADD_IMAGE);
         });
-
 
         imageBack.setOnClickListener(v -> onBackPressed());
         imageSetTaskCompleted.setOnClickListener(v -> {
@@ -59,9 +75,40 @@ public class SetTaskCompletedActivity extends AppCompatActivity {
             intent.putExtra("desc", desc);
             intent.putExtra("reward", reward);
             intent.putExtra("pos", pos);
+            uploadFile();
             setResult(RESULT_OK, intent);
             finish();
         });
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private void uploadFile() {
+        if (fileImageUri != null) {
+            StorageReference fileReference = storageReference.child(System.currentTimeMillis()
+            + "." + getFileExtension(fileImageUri));
+            fileReference.putFile(fileImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(SetTaskCompletedActivity.this, "Upload successful", Toast.LENGTH_LONG).show();
+                    UploadFile upload = new UploadFile("picture", taskSnapshot.getMetadata()
+                            .getReference().getDownloadUrl().toString());
+                    String uploadId = databaseReference.push().getKey();
+                    databaseReference.child(uploadId).setValue(upload);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull @NotNull Exception e) {
+                    Toast.makeText(SetTaskCompletedActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            Toast.makeText(this, "No file selected", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
